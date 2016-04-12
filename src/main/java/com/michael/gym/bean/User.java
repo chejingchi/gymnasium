046f1c772b865a3136781a13e9251425442072f8
michaelChe <1020934146@qq.com>
2016-04-12 18:29:12 +0800

    michael:create gymnasium application

--- src/main/java/com/michael/gym/bean/User.java
+++ src/main/java/com/michael/gym/bean/User.java
@@ -39,16 +39,80 @@ public class User {
     private String telephone;
 
     @Column
-    private String anotherTphone;
-
-    @Column
     private String email;
 
     @Column
-    private String booksId;
-
-    private String booksName;
-
-    @Column
     private String qq;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public String getVipCardNo() {
+        return vipCardNo;
+    }
+
+    public void setVipCardNo(String vipCardNo) {
+        this.vipCardNo = vipCardNo;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
 }
